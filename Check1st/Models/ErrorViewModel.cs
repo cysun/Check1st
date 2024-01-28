24e2ce67fc4e015f53791daee2c3e14bd8d47770
Chengyu Sun <csun@calstatela.edu>
2024-01-27 23:16:23 -0800

    Add project files.

---
+++ Check1st/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Check1st.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
